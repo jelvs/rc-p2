27f33c70df5506e74acb13310c1678983979b47a
Tiago Sousa <jelvs13@gmail.com>
2017-12-04 19:31:11 +0000

    errors fixes

--- src/FilePlayer.java
+++ src/FilePlayer.java
@@ -1,14 +1,16 @@
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeMap;
 import java.io.*;
 
 public class FilePlayer extends Thread{
-	
+
 	private static String url;
 	private static final String COCO = "C";
 	private static final String DANTE = "D";
@@ -16,30 +18,32 @@ public class FilePlayer extends Thread{
 	private static Map<Integer, ArrayList<Integer>> avgBand = new HashMap<Integer, ArrayList<Integer>>();
 	private static ArrayList<String> seg = new ArrayList<String>();
 	private static ArrayList<Integer> avgB = new ArrayList<Integer>(); // average bandWidth
+	private static long playoutDelay;
 	private static String TXT = "descriptor.txt";
-	
+
 
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Scanner inn = new Scanner(System.in);
-		System.out.println("Iniciar coco = C || Iniciar dante = d");
+		System.out.println("Iniciar coco = C || Iniciar dante = D");
 		String cmd = getCommand(inn);
 		switch(cmd){
 		case COCO:
-			url = args.length == 1 ? args[0] : "http://localhost:8080/coco" ; 	
+			url = args.length == 1 ? args[0] : "http://localhost:8080/coco/" ; 	
 			break;
 		case DANTE:
-			url = args.length == 1 ? args[0] : "http://localhost:8080/dante" ;
+			url = args.length == 1 ? args[0] : "http://localhost:8080/dante/" ;
 			break;
 		default:
 			break;
 		}
+		playoutDelay = args.length == 2 ? Integer.parseInt(args[1]) : 10;
 		URL u = new URL(url);
-		
-	
-		
-		
+
+
+
+
 
 		System.out.println("\n========================================\n");
 		System.out.println("Processing url: "+url+"\n");
@@ -60,8 +64,8 @@ public class FilePlayer extends Thread{
 		InetAddress serverAddr = InetAddress.getByName(u.getHost());
 		int port = u.getPort();
 		if ( port == -1 ) port = 80;
-		String descriptor = u.getPath().concat(TXT);
 		Socket sock = new Socket( serverAddr, port );
+		String descriptor = u.getPath().concat(TXT);
 		OutputStream toServer = sock.getOutputStream();
 		InputStream fromServer = sock.getInputStream();
 
@@ -78,6 +82,8 @@ public class FilePlayer extends Thread{
 
 		System.out.println("Got answer: "+ answerLine +"\n");
 
+
+
 		String[] result = parseHttpReply(answerLine);
 		answerLine = readLine(fromServer);
 		while ( !answerLine.equals("") ) {
@@ -85,38 +91,112 @@ public class FilePlayer extends Thread{
 			answerLine = readLine(fromServer);
 		}
 
+
 		//Structure to read descriptor.txt
+
 
 		BufferedReader in = new BufferedReader (new InputStreamReader(fromServer));
 		String content = "";
-		
-		
 
 
 		while ((content = in.readLine()) != null) {
-			String[] tmp = content.split("/");
-			
+			String[] tmp = content.split(" ");
+			String[] aux;
 			if(!content.equals("")) {
 				if(content.startsWith("video") ) {
-					seg.add(tmp[2]);
-					segments.put(Integer.parseInt(tmp[1]), seg );
 						
+					tmp[0].split("/");
+					aux = tmp[0].substring(8).split("\\.m4s");
+					seg.add(aux[0]);
+
+					segments.put(Integer.parseInt(tmp[1]), seg );
+
 				}	
 			}
 
 		}
-	
+
+
 
 		System.out.println("\n========================================");
 		System.out.println("\nGot an empty line, showing body \n");
 		System.out.println("========================================");
+
+		System.out.println(seg.get(5));
+
 		int c ;
 		while( (c = fromServer.read() ) > 0 ) {
 			System.out.print((char) c);
 		}
+
 		System.out.println();
+		in.close();
 		sock.close();
+
+
+
+
+
+
+
 	}
+
+
+
+
+	//supostamente pede segmento a segmento vê o que achas da ideia
+	private static void processHTTPrequest(InputStream in, OutputStream out) {
+		try {
+
+			String request = readLine(in);
+			System.out.println( "received: "+request );
+			String[] requestParts = parseHttpRequest(request);
+			// ignora-se o resto da mensagem HTTP
+			while ( ! readLine( in ).equals("") );
+			// tratamento do pedido
+			if( requestParts[0].equalsIgnoreCase("GET") ) {
+				sendSegment(requestParts[1] , out);
+			}
+
+			else { // ignore other requests
+				dumpStream (notSupportedPageStream(),out);
+			}
+
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	//Enviar o segmento em https
+	private static void sendSegment(String segment, OutputStream out) throws IOException{
+		boolean sended = false;
+		int i = 0;
+
+		while(i < seg.size()) {
+			if(seg.get(i).equals(segment)) {
+				//falta escolher a avgband
+				System.out.println(segment.length());
+				StringBuilder reply = new StringBuilder("HTTP/1.0 200 OK\r\n");
+				reply.append("Date: "+new Date().toString()+"\r\n");
+				reply.append("Server: The tiny server (v0.9) \r\n" );
+				reply.append("Content-Length: "+ segment.length() +"\r\n\r\n");
+
+				out.write(reply.toString().getBytes());
+
+				out.write( segment.getBytes(), 0, segment.length());
+				sended = true;
+			}
+		}
+		if(!sended) {
+			StringBuilder reply = new StringBuilder("HTTP/1.0 404 NOT FOUND\r\n");
+			reply.append("\r\n");
+			//reply.append("Date: "+new Date().toString()+"\r\n");
+			//reply.append("Server: "+"The tiny server (v0.9)"+"\r\n");
+			//reply.append("Content-Length: "+String.valueOf(length)+"\r\n\r\n");
+			out.write(reply.toString().getBytes());
+		}
+	}
+
 
 
 	private static String getCommand(Scanner inn) {
@@ -124,7 +204,7 @@ public class FilePlayer extends Thread{
 		input = inn.next().toUpperCase();
 		return input;
 	}
-	
+
 	private static void getAvgBand() {
 		if(segments.containsKey(1)) {
 			//position 0 = coco
@@ -157,13 +237,13 @@ public class FilePlayer extends Thread{
 			//position 0 = coco
 			//position 1 = dante
 			avgB.add(2125252);
-				
+
 			avgBand.put(5, avgB);
 		}
-	
+
 	}
-	
-	
+
+
 
 	/**
 	 * Reads one message from the HTTP header
@@ -178,6 +258,36 @@ public class FilePlayer extends Thread{
 		}
 		return sb.toString() ;
 	} 
+
+	static void dumpStream( InputStream in, OutputStream out)
+			throws IOException {
+		byte[] arr = new byte[1024];
+		for( ;;) {
+			int n = in.read( arr);
+			if( n == -1)
+				break;
+			out.write( arr, 0, n);
+		}
+	}
+
+
+
+
+
+	/**
+	 * Returns an input stream with an error message "Not Implemented"
+	 */
+	static InputStream notSupportedPageStream() {
+		final String page = 
+				"<HTML><BODY>Request Not Supported</BODY></HTML>" ;
+		int length = page.length();
+		StringBuilder reply = new StringBuilder("HTTP/1.0 501 Not Implemented\r\n");
+		reply.append("Date: "+new Date().toString()+"\r\n");
+		reply.append("Server: "+"The tiny server (v0.9)"+"\r\n");
+		reply.append("Content-Length: "+String.valueOf(length)+"\r\n\r\n");
+		reply.append( page );
+		return new ByteArrayInputStream( reply.toString().getBytes());
+	}
 
 
 	/**
---
+++ src/Segment.java
@@ -0,0 +1,26 @@
+
+public class Segment {
+
+	private final int quality;;
+	private final String seg;
+	private final int dimension;
+	
+	public Segment(int quality, String seg, int dimension) {
+		this.quality = quality;
+		this.seg = seg;
+		this.dimension = dimension;
+	}
+	
+	public int getQuality() {
+		return quality;
+	}
+	
+	public String getSeg() {
+		return seg;
+	}
+	
+	public int getDimension() {
+		return dimension;
+	}
+	
+}
